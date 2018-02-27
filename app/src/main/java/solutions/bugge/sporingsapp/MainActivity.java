d8397f6961d380282d4ee82f6d77e31da72c7ac5
Bugge Solutions <erik@bugge.solutions>
2018-02-27 19:43:49 +0100

    Added contact layout/activity

--- app/src/main/java/solutions/bugge/sporingsapp/MainActivity.java
+++ app/src/main/java/solutions/bugge/sporingsapp/MainActivity.java
@@ -5,9 +5,12 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -24,11 +27,33 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private void setErrorText(String message){
+        TextView txtError = findViewById(R.id.txtError);
+        txtError.setText(message);
+    }
+
+    private void btnSearchAction(){
+        Button btnSearch = findViewById(R.id.btnSearch);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText txtSeach = findViewById(R.id.txtSearch);
+                final int numSearch = Integer.parseInt(txtSeach.getText().toString());
+                if (numSearch < 100){
+                    setErrorText("Koden din er for kort");
+                }
+                else {
+                    // TODO
+                }
+            }
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnSearchAction();
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
