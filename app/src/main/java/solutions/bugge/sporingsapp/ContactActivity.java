2ca340204671383f635ebcb4388ae9b026beae39
Bugge Solutions <erik@bugge.solutions>
2018-03-03 00:24:48 +0100

    Added dummy data
    TODO: Connect to a MySQl database

--- app/src/main/java/solutions/bugge/sporingsapp/ContactActivity.java
+++ app/src/main/java/solutions/bugge/sporingsapp/ContactActivity.java
@@ -5,9 +5,13 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class ContactActivity extends AppCompatActivity {
+
+    TextView phoneNumber;
 
     /**
      * Listener for navigation items. Starts MainActivity.
@@ -36,6 +40,9 @@ public class ContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        phoneNumber = findViewById(R.id.txtPhoneNumberView);
+        Linkify.addLinks(phoneNumber, Linkify.ALL);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
--- app/src/main/java/solutions/bugge/sporingsapp/TraceActivity.java
+++ app/src/main/java/solutions/bugge/sporingsapp/TraceActivity.java
@@ -4,23 +4,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.TextView;
 
 public class TraceActivity extends AppCompatActivity {
 
     WebView mWebView;
-    Double testCoordsLang;
-    Double testCoordsLong;
+    TextView parcelLocationView;
+    TextView parcelDeliveryTimeView;
+    String parcelLocation;
+    String parcelDeliveryTime;
+    Double coordsLang;
+    Double coordsLong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trace);
 
+        // Create dummy data
+        Double testCoordsLang = 62.7385103;
+        Double testCoordsLong = 8.1021172;
+        String testParcelLocation = "Buggestranda";
+        String testParcelDeliveryTime = "12:00 - 01/03/2018";
+
+        // Set dummy data
+        coordsLang = testCoordsLang;
+        coordsLong = testCoordsLong;
+        parcelLocation = testParcelLocation;
+        parcelDeliveryTime = testParcelDeliveryTime;
+
+        parcelLocationView = findViewById(R.id.txtLocationInfoView);
+        parcelDeliveryTimeView = findViewById(R.id.txtDeliveryInforView);
+        parcelLocationView.setText(parcelLocation);
+        parcelDeliveryTimeView.setText(parcelDeliveryTime);
+
         mWebView = findViewById(R.id.webParcelLocationView);
         WebSettings mWebViewSettings = mWebView.getSettings();
         mWebViewSettings.setJavaScriptEnabled(true);
-        testCoordsLang = 62.7385103;
-        testCoordsLong = 8.1021172;
-        mWebView.loadUrl("http://maps.googleapis.com/maps/api/staticmap?center=" + testCoordsLang + "," + testCoordsLong + "&zoom=14&format=png&sensor=false&size=500x500&maptype=hybrid");
+        mWebView.loadUrl("http://maps.googleapis.com/maps/api/staticmap?center=" + coordsLang + "," + coordsLong + "&zoom=14&format=png&sensor=false&size=500x500&maptype=hybrid");
     }
 }
